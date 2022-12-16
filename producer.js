26a966e18f11ec243d6c45f23566de4d8cd555ce
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-12-15 23:38:48 -0300

    After

---
+++ producer.js
@@ -0,0 +1,41 @@
+import { Kafka } from 'kafkajs'
+import { randomUUID } from 'node:crypto'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function boostrap() {
+  const kafka = new Kafka({
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: [`${process.env.KAFKA_BROKER_1}`],
+    sasl: {
+      mechanism: 'scram-sha-256',
+      username:
+        `${process.env.KAFKA_USERNAME}`,
+      password:
+        `${process.env.KAFKA_PASSWORD}`,
+    },
+    ssl: true,
+  })
+
+  const producer = kafka.producer()
+
+  await producer.connect()
+  
+  await producer.send({
+    topic: 'notifications.send-notification',
+    messages: [
+      {
+        value: JSON.stringify({
+          content: 'Nova solicitação de amizade!',
+          category: 'social',
+          recipientId: randomUUID(),
+        })
+      }
+    ],
+  })
+
+  await producer.disconnect()
+}
+
+boostrap()
